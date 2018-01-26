5d73e8a71d63bc2e695dfe3dc84d595dc9ef8225
Guest User <Guest@Epicodus-10E.local>
2018-01-26 14:57:27 -0800

    add math for pizza size and toppings, update order total with that math on submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,46 @@
 //Back-end
-//Pizza constructor
-Pizza.prototype.addToppings = $('.chk:checked').each(function() {
-  this.toppings.push($('.chk:checked').val());
+
+Order.prototype.sumPizzas = function() {
+  var pizzaPrice = 0;
+  console.log(this.pizzas);
+  for(i = 0; i < this.pizzas.length; i++) {
+    var sizeString = this.pizzas[i].size
+    var numToppings = this.pizzas[i].toppings
+    if (sizeString === "large") {
+      pizzaPrice = pizzaPrice + 10 + numToppings.length;
+    } else if (sizeString === "medium") {
+      console.log(this.pizzas[i].size);
+      pizzaPrice = pizzaPrice + 7 + numToppings.length;
+    } else {
+      pizzaPrice = pizzaPrice + 5 + numToppings.length;
+    }
+  }
+  this.total = pizzaPrice;
+  console.log(this.total);
+};
+
+Pizza.prototype.addToppings = (function() {
+  var toppingsChecked = [];
+  $('.chk:checked').each(function() {
+      toppingsChecked.push($(this).val());
+    })
+  this.toppings = toppingsChecked;
   });
 
-Order.prototype.addPizza = function() {
+Order.prototype.addPizza = function(Pizza) {
   this.pizzas.push(Pizza);
 };
 
+//Pizza constructor
 function Pizza(size) {
   this.size = size;
   this.toppings = [];
-}
+};
 
 function Order(){
   this.pizzas = [];
   this.total = 0;
-}
+};
 
 
 
@@ -24,15 +48,17 @@ function Order(){
 $(document).ready(function() {
   var newOrder = new Order();
   console.log(newOrder);
-  $('#pizzaForm').submit(function(event) {
+  $('#addPizza').click(function(event) {
     event.preventDefault();
     var newPizza = new Pizza($('input[name=size]:checked', '#pizzaForm').val());
-    // newPizza.addToppings();
-    newOrder.addPizza();
+    newPizza.addToppings();
+    newOrder.addPizza(newPizza);
     console.log(newOrder);
     console.log(newPizza);
   });
-  $('#pizzaForm').submit(function(event) {
+  $('#submitOrder').click(function(event) {
+    event.preventDefault();
+    newOrder.sumPizzas();
 
   });
 });
